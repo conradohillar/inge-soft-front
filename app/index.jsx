1dcfd0a0faa954f52a249d33ac34399a958678f2
conradohillar <hillarconrado@gmail.com>
2024-09-04 16:43:05 -0300

    working Tamagui import

--- app/index.jsx
+++ app/index.jsx
@@ -1,21 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, useColorScheme } from 'react-native';
 import { Link } from 'expo-router';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import { Paragraph, Spacer, TamaguiProvider, Theme, YStack } from "tamagui";
+import config from "../tamagui.config";
 
 export default function App() {
-  return (
-    <SafeAreaView className={"bg-primary h-full"}>
-        <Link href="/home" style={{ color: 'blue'}}>Go to home</Link>
-    </SafeAreaView>
+    const colorScheme = useColorScheme();
+    return (
+        <TamaguiProvider config={config}>
+            <Theme name={colorScheme === "dark" ? "dark" : "light"}>
+                <SafeAreaView className={"bg-primary h-full"}>
+                    <Link href="/home" style={{ color: 'blue'}}>Go to home</Link>
+                </SafeAreaView>
+            </Theme>
+        </TamaguiProvider>
   );
 }
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     backgroundColor: '#fff',
-//     alignItems: 'center',
-//     justifyContent: 'center',
-//   },
-// });
