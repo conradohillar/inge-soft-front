0c15b9a8bf7ffd7f8d79ff14ea322761d9eda8ce
EzequielTestoni <ezetestoni@gmail.com>
2024-09-06 11:18:29 -0300

    Home page with map

--- app/(tabs)/home.jsx
+++ app/(tabs)/home.jsx
@@ -1,23 +1,32 @@
-import { View, Text } from 'react-native'
+import { View, Text, Image } from 'react-native'
 import React from 'react'
 import { Input, XStack, YStack } from 'tamagui'
 import BlackButton from '../../components/BlackButton'
+import { Link } from 'expo-router'
 
 const Home = () => {
   return (
     <YStack className="flex-1 items-center justify-evenly bg-primary">
-      <View className="flex-1 items-center justify-center">
-        <Text className="text-[30px] font-qsemibold text-black">Buscá tu viaje</Text>
-      </View>
-      <XStack className="flex-1 items-center justify-center">
-        <Input className='px-4' placeholder="Buscar..." style={{backgroundColor:"#EEEEEE", width:300}} />
+       <XStack className="flex-1 items-center justify-between ">
+          <Text className="text-[12px] font-qsemibold text-secondary">¿Querés publicar un viaje?</Text>
+          <BlackButton height={52} width={160}>
+            <Text className="text-[15px] font-qsemibold text-primary">Publicar viaje</Text>
+          </BlackButton>
       </XStack>
-      <YStack className="flex-2 items-center justify-evenly">
-        <BlackButton height={55} width={250}>
-          <Text className="text-[22px] font-qsemibold text-primary">Publicar viaje</Text>
-        </BlackButton>
-        <Text className="text-[15px] font-qsemibold text-secondary">¿Querés publicar un viaje?</Text>
+      <YStack className="flex-4 items-center justify-center">
+        <View className="flex-3 items-center justify-center py-5">
+          <Image source={require("../../assets/images/map.png")} style={{height: 300 , width: 300, borderRadius:35}}/>
+        </View>
+        <Input className='px-4' placeholder="Buscar..." style={{backgroundColor:"#EEEEEE", width:300}} />
       </YStack>
+      <View className="flex-1 items-center justify-center">
+        <Link href="/searchTrip" asChild>
+          <BlackButton height={70} width={250}>
+            <Text className="text-[20px] font-qsemibold text-primary">Buscar viaje</Text>
+          </BlackButton>
+        </Link>
+      </View>
+     
     </YStack>
   )
 }
