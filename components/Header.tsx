f8d0b674864840865376d33e5f34f72e31adfe9d
EzequielTestoni <ezetestoni@gmail.com>
2024-09-07 00:49:26 -0300

    Header link to home

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,4 +1,5 @@
 import { Menu } from "@tamagui/lucide-icons";
+import { Link } from "expo-router";
 import { XStack, Text, YStack } from "tamagui";
 
 
@@ -6,7 +7,9 @@ export default function Header(){
     return (
         <XStack className="min-h-[90px] w-full items-center justify-between px-4" style={{borderBottomWidth: 2, borderBottomColor: '#ccc'}}>
             <Menu size="$2.5" color={"black"}/>
-            <Text className="text-6xl font-qsemibold text-secondary mt-2">rydio</Text>
+            <Link href="/(tabs)/home" asChild>
+                <Text className="text-6xl font-qsemibold text-secondary mt-2">rydio</Text>
+            </Link>
         </XStack>
     );
 }
