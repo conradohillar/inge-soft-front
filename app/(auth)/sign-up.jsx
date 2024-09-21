adab9371923b4214187485b0d06bcc9cfc9ffc51
santiagoBassi <santiagobassi21@gmail.com>
2024-09-21 19:42:13 -0300

    register working

--- app/(auth)/sign-up.jsx
+++ app/(auth)/sign-up.jsx
@@ -3,33 +3,25 @@ import React, {useState} from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import { YStack } from 'tamagui'
 import CustomInput from '../../components/CustomInput'
-import BlackButton from '../../components/BlackButton'
+import ButtonNext from '../../components/ButtonNext'
 import { Link, useRouter } from 'expo-router'
+import AutocompleteCityInput from '../../components/AutocompleteCityInput'
 
 export default function SignUp(){
+  const [userName, setUserName] = useState('');
   const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [confirmPassword, setConfirmPassword] = useState('');
-
-  const handleConfirmPasswordChange = (value) => {
-    setConfirmPassword(value);
-
-    // Comparar contraseñas
-    if (password !== value) {
-      // Handle error
-    } else {
-     // Continue
-    }
-  };
-
+  const [address, setAddress] = useState('');
+  
+    
   const router = useRouter();
+  
   const handleContinue = async () => {
     
     try {
 
       router.push({
         pathname: "/(auth)/sign-up2",
-        params: { email, password}
+        params: { email, userName, address }
       });
     } catch (error) {
       console.error("Error: ", error);
@@ -43,14 +35,16 @@ export default function SignUp(){
           <Text className="text-primary text-5xl font-qbold">TU CUENTA</Text>
         </YStack>
         <YStack className="items-center justify-center">
+          <CustomInput title="Nombre" value={userName} handleChangeText={setUserName}/>
+          <AutocompleteCityInput title="Dirección" placeholder="i.e: Tigre" value={address} onChangeText={setAddress}/>
           <CustomInput title="E-mail" value={email} handleChangeText={setEmail}/>
-          <CustomInput title="Ingresá tu contraseña" value={password} handleChangeText={setPassword}/>
-          <CustomInput title="Confirmá tu contraseña" value={confirmPassword} handleChangeText={handleConfirmPasswordChange}/>
+          
+          
         </YStack>
         <YStack className="items-center">
-          <BlackButton href="/(auth)/sign-up2" height={60} width={220}>
-            <Text className="text-white text-xl font-qsemibold">Continuar</Text>
-          </BlackButton>
+          <ButtonNext height={90} width={270} onPress={handleContinue}>
+                    <Text className="text-2xl font-qsemibold text-white">Continuar</Text>
+          </ButtonNext>
           <Link href="/(pages)/LandingPage" asChild>
             <Text className="text-base text-red-500 font-qsemibold underline">Cancelar</Text>
           </Link>
--- app/(auth)/sign-up2.jsx
+++ app/(auth)/sign-up2.jsx
@@ -1,49 +1,102 @@
 import { View, Text, Image } from 'react-native'
-import React from 'react'
+import React, { useState } from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import { Avatar, Button, XStack, YStack } from 'tamagui'
 import CustomInput from '../../components/CustomInput'
-import BlackButton from '../../components/BlackButton'
-import { Link } from 'expo-router'
-import icons from "../../constants/icons"
+import ButtonNext from '../../components/ButtonNext'
+import { Link, useLocalSearchParams } from 'expo-router'
+import { set } from 'date-fns'
+import Home from '../(tabs)/home'
+import { QueryClient, QueryClientProvider, useQuery, useMutation } from '@tanstack/react-query'
+import axios from 'axios';
+
+import { LOCAL_IP } from '@env'
+
+
+const queryClient = new QueryClient()
+
+
 
 const SignUp2 = () => {
   return (
-    <SafeAreaView className="bg-background h-full w-full">
-      <YStack className="h-full justify-evenly">
-        <YStack className="items-center">
-          <Text className="text-black text-4xl font-qbold">Detalles</Text>
-          <Text className="text-black text-3xl font-qsemibold">de 
-            <Text className="text-primary text-3xl font-qbold"> TU PERFIL</Text>
-          </Text>
-        </YStack>
-        <YStack className="items-center">
-          <Avatar circular size="$12" borderColor="$black" borderWidth={1}>
-            <Avatar.Image
-              accessibilityLabel="Cam"
-              src="https://images.unsplash.com/photo-1548142813-c348350df52b?&w=150&h=150&dpr=2&q=80"
-            />
-            <Avatar.Fallback backgroundColor="$gray5" />
-          </Avatar>
-          <Text className="text-sm text-gray-500 font-qsemibold mt-3">Agregá una foto de perfil</Text>
-        </YStack>
-        <YStack className="justify-center">
-          <CustomInput title="Nombre de usuario"/>
-          <CustomInput title="Número de teléfono (opcional)"/>
-        </YStack>
-        <XStack className="w-full items-center justify-evenly">
-          <Link href="/(auth)/sign-up" asChild>
-            <Button className="bg-background">
-              <Image source={icons.arrowleft} className="h-7 w-7" resizeMode='contain'/>
-            </Button>
-          </Link>
-          <BlackButton href="/(tabs)/home">
-            <Text className="text-white text-xl font-qsemibold">Ir al Inicio</Text>
-          </BlackButton>
-        </XStack>
-      </YStack>
-    </SafeAreaView>
+    <QueryClientProvider client={queryClient}>
+      <Content />
+    </QueryClientProvider>
   )
+
 }
 
 export default SignUp2
+
+
+
+function Content() {
+  
+  const { email, userName, address } = useLocalSearchParams();
+
+  const [dni, setDni] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+
+  const [error, setError] = useState(false);
+
+  const userData =   { 
+  "name": userName,
+  "email": email,
+  "password": password,
+  "address": address,
+  "dni": dni,
+  "photo_id": 1
+};
+
+  const mutation = useMutation({
+    mutationFn: (data) => {
+      return axios.post(`http://${LOCAL_IP}:8000/auth/users/register`, data)
+    },
+  })
+
+  const handleContinue = () => {
+
+    if (password !== confirmPassword) {
+      setError(true);                     //ACA PODEMOS PONER UN RENDERIZADO CONDICIONAL DE LOS ERRORES BAJO LA CONDICION DE ESTE FLAG
+    } else {
+      console.log(userData)
+      mutation.mutate(userData)
+    }
+  };
+  return (
+    <SafeAreaView className="bg-background h-full w-full">
+      {mutation.isLoading ? (<LoadingPage />) : (
+        <>
+          {mutation.isError ? (
+            <Text>An error occurred: {mutation.error.message}</Text>   // ACA TENEMOS QUE VER QUE MOSTRAMOS
+          ) : null}
+
+          {mutation.isSuccess ? <Home /> : null}
+          <YStack className="h-full justify-evenly">
+            <YStack className="items-center">
+              <Text className="text-black text-4xl font-qbold">Detalles</Text>
+              <Text className="text-black text-3xl font-qsemibold">de
+                <Text className="text-primary text-3xl font-qbold"> TU PERFIL</Text>
+              </Text>
+            </YStack>
+
+            <YStack className="justify-center">
+              <CustomInput title="DNI" value={dni} handleChangeText={setDni} />
+              <CustomInput title="Contraseña" value={password} handleChangeText={setPassword} />
+              <CustomInput title="Confirmá tu contraseña" value={confirmPassword} handleChangeText={setConfirmPassword} />
+            </YStack>
+            <YStack className="items-center">
+              <ButtonNext height={90} width={270} onPress={handleContinue}>
+                <Text className="text-2xl font-qsemibold text-white">Registrate</Text>
+              </ButtonNext>
+              <Link href="/(pages)/LandingPage" asChild>
+                <Text className="text-base text-red-500 font-qsemibold underline">Cancelar</Text>
+              </Link>
+            </YStack>
+          </YStack>
+        </>
+      )}
+    </SafeAreaView>
+  )
+}
