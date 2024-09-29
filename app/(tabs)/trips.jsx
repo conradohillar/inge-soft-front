8dd4fefe6160d592a95b1f0456562ef6c187bc9b
EzequielTestoni <ezetestoni@gmail.com>
2024-09-29 12:06:42 -0300

    Added tabs to Trips tab

--- app/(tabs)/trips.jsx
+++ app/(tabs)/trips.jsx
@@ -2,12 +2,13 @@ import { StyleSheet, Text, View } from 'react-native'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import Header from '../../components/Header';
 import { YStack } from 'tamagui';
-
+import HorizontalTabs from '../../components/HorizontalTabs';
 const Trips = () => {
   return (
     <SafeAreaView className="h-full w-full bg-background">
       <Header />
-      <YStack className="h-full items-center justify-evenly bg-background">      
+      <YStack className="h-full items-center justify-evenly bg-background">
+        <HorizontalTabs />      
       </YStack>
     </SafeAreaView>
   )
