6500f1853c30dad4512003921fad4537bea4d5ed
santiagoBassi <santiagobassi21@gmail.com>
2024-10-13 17:41:45 -0300

    testing sing-in finished

--- __tests__/Sign-in.test.jsx
+++ __tests__/Sign-in.test.jsx
@@ -4,6 +4,11 @@ import SignIn from '../app/(auth)/sign-in';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { TamaguiProvider } from "tamagui";
 import config from "../tamagui.config";
+import axios from 'axios';
+
+
+
+jest.mock('axios');
 
 const queryClient = new QueryClient();
 const Provider = ({ children }) => (
@@ -13,6 +18,17 @@ const Provider = ({ children }) => (
 );
 
 describe('SignIn component', () => {
+    const user = [{
+        "name": "Santiago Bassi",
+        "email": "santiagobassi21@gmail.com",
+        "address": "Chascomús, Partido de Chascomús, Buenos Aires, Argentina",
+        "dni": 123456789,
+        "photo_url": "https://i.ibb.co/WxTJXCZ/de12caf4398b.jpg",
+        "is_driver": true
+    }];
+
+    axios.get.mockResolvedValue(user);
+
     it('should render the component', async () => {
         const { getByPlaceholderText } = render(<Provider><SignIn /></Provider>);
 
@@ -42,5 +58,48 @@ describe('SignIn component', () => {
         });
     });
 
+    it('should display error message for non-existent email and password', async () => {
+        axios.post.mockRejectedValue({
+            response: {
+                status: 401,
+                data: { message: 'E-mail o contrasena invalidos.' }
+            }
+        });
 
+        const { getByText, getByPlaceholderText } = render(<Provider><SignIn /></Provider>);
+
+        const emailInput = getByPlaceholderText("Ingresá tu e-mail");
+        const passwordInput = getByPlaceholderText("Ingresá tu contraseña");
+        const submitButton = getByText("Ir al Inicio");
+
+        fireEvent.changeText(emailInput, "nonexistent@example.com");
+        fireEvent.changeText(passwordInput, "wrongpassword");
+        fireEvent.press(submitButton);
+
+        await waitFor(() => {
+            expect(getByText("E-mail o contrasena invalidos.")).toBeTruthy();
+        });
+    });
+    it('should display error message for non-existent email and password', async () => {
+        axios.post.mockRejectedValue({
+            response: {
+                status: 408,
+                data: { message: 'Axios error. Timeout' }
+            }
+        });
+
+        const { getByText, getByPlaceholderText } = render(<Provider><SignIn /></Provider>);
+
+        const emailInput = getByPlaceholderText("Ingresá tu e-mail");
+        const passwordInput = getByPlaceholderText("Ingresá tu contraseña");
+        const submitButton = getByText("Ir al Inicio");
+
+        fireEvent.changeText(emailInput, "nonexistent@example.com");
+        fireEvent.changeText(passwordInput, "wrongpassword");
+        fireEvent.press(submitButton);
+
+        await waitFor(() => {
+            expect(getByText("Error de conexion, intente mas tarde.")).toBeTruthy();
+        });
+    });
 });
