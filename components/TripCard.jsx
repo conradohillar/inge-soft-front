6d207175a41820f73cbe9337a9dcd1da346856bc
conradohillar <hillarconrado@gmail.com>
2024-11-27 14:30:38 -0300

    fixed Trips tab ui

--- components/TripCard.jsx
+++ components/TripCard.jsx
@@ -1,6 +1,24 @@
 import { Image, View, Text, Pressable } from "react-native";
-import { Button, XStack, YStack, Avatar } from "tamagui";
+import { Avatar, XStack, YStack } from "tamagui";
 import icons from "../constants/icons";
+
+const getStateIcon = (state) => {
+  const icons = {
+    pending: {
+      source: require("../assets/icons/alert.png"),
+      color: "#ff6633",
+    },
+    accepted: {
+      source: require("../assets/icons/accepted.png"),
+      color: "#008000",
+    },
+    dismissed: {
+      source: require("../assets/icons/dismissed.png"),
+      color: "#FF0000",
+    },
+  };
+  return icons[state];
+};
 
 export default function TripCard({
   from,
@@ -27,6 +45,14 @@ export default function TripCard({
           borderRadius: 40,
           opacity: pressed ? 0.7 : 1,
           backgroundColor: pressed ? "#cdddcd" : "#eee",
+          shadowColor: "#000",
+          shadowOffset: {
+            width: 0,
+            height: 2,
+          },
+          shadowOpacity: 0.25,
+          shadowRadius: 3.84,
+          elevation: 5,
         })}
         onPress={() => handleOpenDetail(ride_id)}
       >
@@ -43,33 +69,27 @@ export default function TripCard({
               <Text className="text-2xl font-qbold text-black"> {to}</Text>
             </XStack>
             <Avatar circular size="$7" borderColor="$black" borderWidth={1}>
-              <Avatar.Image
-                src={url === "" ? icons.placeholder_profile : url}
-              />
+              <Avatar.Image src={url || icons.placeholder_profile} />
               <Avatar.Fallback backgroundColor="$gray8" />
             </Avatar>
           </XStack>
+
           <YStack>
             <Text className="text-base font-qsemibold text-grey-100">
               Conductor: {driver}
             </Text>
-            <Text className="text-basefont-qsemibold text-grey-100 mb-3">
+            <Text className="text-base font-qsemibold text-grey-100 mb-3">
               {date}
             </Text>
             <XStack className="items-center justify-between">
               <Text className="text-xl font-qbold text-grey-800">${price}</Text>
               {state && (
                 <Image
-                  source={getIcon(state)}
+                  source={getStateIcon(state).source}
                   className="w-10 h-10 mr-4"
                   resizeMode="contain"
                   style={{
-                    tintColor:
-                      state === "pending"
-                        ? "#ff6633"
-                        : state === "accepted"
-                        ? "#008000"
-                        : "#FF0000",
+                    tintColor: getStateIcon(state).color,
                   }}
                 />
               )}
@@ -80,13 +100,3 @@ export default function TripCard({
     </View>
   );
 }
-
-const getIcon = (state) => {
-  if (state === "pending") {
-    return require("../assets/icons/alert.png");
-  } else if (state === "accepted") {
-    return require("../assets/icons/accepted.png");
-  } else if (state === "dismissed") {
-    return require("../assets/icons/dismissed.png");
-  } else return null;
-};
